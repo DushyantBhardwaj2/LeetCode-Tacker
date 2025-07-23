e19e297621038f8435908bdbe30f2b2b2562523a
Dushyant Bhardwaj <dushyant.bhardwaj.ug23@nsut.ac.in>
2025-07-23 16:18:48 +0530

    Time: 305 ms (19.31%) | Memory: 108.9 MB (28.83%) - LeetSync

---
+++ DP1D/2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq;
+        for(int i=0;i<piles.size();i++){
+            pq.push(piles[i]);
+        }
+        while(k--){
+           int n=pq.top();
+           pq.pop() ;
+           n=(n+1)/2;
+           pq.push(n);
+        }
+        int sum=0;
+        while(!pq.empty()){
+            sum+=pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
