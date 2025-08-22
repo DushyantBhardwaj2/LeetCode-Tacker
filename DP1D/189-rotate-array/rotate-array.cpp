9f9dc2ff14c3b1e8dd7d320d4ab83d2714fa51e5
Dushyant Bhardwaj <dushyant.bhardwaj.ug23@nsut.ac.in>
2025-08-22 10:27:02 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (98.14%) - LeetSync

---
+++ DP1D/189-rotate-array/rotate-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void reverseArray(vector<int>& nums, int s,int e){
+        while(s<e){
+            swap(nums[s],nums[e]);
+            s++;
+            e--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        
+        reverseArray(nums,0,n-1);
+        reverseArray(nums,0,k-1);
+        reverseArray(nums,k,n-1);
+
+        
+    }
+};
